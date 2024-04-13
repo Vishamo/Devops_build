f00198b8c2d33a041ef7d130b15b86260eab418d
vishalmodanwal <vishalmodanwal@IN-9K1D5S3>
2024-04-13 22:53:13 +0530

    Add project files.

---
+++ Devops_build/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Devops_build.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Devops_build.Controllers
+{ 
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(UserInfo userInfo)
+        {
+            return View("Display", userInfo);
+        }
+    }
+}
